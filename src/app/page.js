d32ff20d0ea771402c1602e5c1f61f711f1ce0df
Stanislav <yanut995@gmail.com>
2025-02-25 14:40:07 +0300

    Убрал задвоенный футер

--- src/app/page.js
+++ src/app/page.js
@@ -6,26 +6,8 @@ import Main from "@/pages/Main";
 export default function Home() {
   return (
     <div className={styles.page}>
-      {/* <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/logoBlue.svg"
-          alt="ЛогоМосОблЭнерго"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-           Мы начали работать над страничкой с Внеплановыми отключениями
-          </li>
-          <li>Мы продолжим работать над страничкой с Внеплановыми отключениями</li>
-        </ol>
-
-
-      </main> */}
       <Main />
-      <AppFooter className={styles.footer} />
+      {/* <AppFooter className={styles.footer} /> */}
     </div>
   );
 }
