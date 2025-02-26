2cff3dee8e7e136b5c280d52d72f90e04db9f11a
Stanislav <yanut995@gmail.com>
2025-02-26 11:00:46 +0300

    Добавил лого и даты в заголовках

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -11,7 +11,7 @@ import {
   Button,
   Switch,
 } from "antd";
-import useAuthStore from "../stores/authStore";
+import useAuthStore from "../../stores/authStore";
 
 const { Title, Text } = Typography;
 const { Panel } = Collapse;
@@ -46,6 +46,17 @@ function formatDateTime(dateTimeStr) {
   const hh = String(dateObj.getUTCHours()).padStart(2, "0");
   const min = String(dateObj.getUTCMinutes()).padStart(2, "0");
   return `${dd}.${mm}.${yyyy} ${hh}:${min}`;
+}
+
+function getPanelHeader(incident) {
+  if (incident.status_incident?.trim() === "выполнена") {
+    return `ТН №${incident.id} ${formatDate(incident.start_date)} ${formatTime(
+      incident.start_time
+    )} - ${formatDate(incident.end_date)} ${formatTime(incident.end_time)}`;
+  }
+  return `ТН №${incident.id} ${formatDate(incident.start_date)} ${formatTime(
+    incident.start_time
+  )}`;
 }
 
 export default function MainContent() {
@@ -114,7 +125,7 @@ export default function MainContent() {
       children: (
         <Collapse>
           {activeIncidents.map((incident) => (
-            <Panel key={incident.id} header={`ТН №${incident.id}`}>
+            <Panel key={incident.id} header={getPanelHeader(incident)}>
               <IncidentDetails incident={incident} />
             </Panel>
           ))}
@@ -127,9 +138,10 @@ export default function MainContent() {
       children: (
         <Collapse>
           {completedIncidents.map((incident) => (
-            <Panel key={incident.id} header={`ТН №${incident.id}`}>
+            <Panel key={incident.id} header={getPanelHeader(incident)}>
               <IncidentDetails incident={incident} />
             </Panel>
+
           ))}
         </Collapse>
       ),
@@ -296,210 +308,3 @@ function IncidentDetails({ incident }) {
     </Row>
   );
 }
-
-// "use client";
-// import React, { useEffect, useState } from "react";
-// import {
-//   Spin,
-//   Alert,
-//   Tabs,
-//   Collapse,
-//   Typography,
-//   Row,
-//   Col,
-//   Button,
-// } from "antd";
-// import useAuthStore from "../stores/authStore";
-// import Container from "../components/Container";
-// import styles from "./Incidents.module.css";
-
-// const { Title } = Typography;
-// const { Panel } = Collapse;
-
-// const MainContent = () => {
-//   const { token, logout } = useAuthStore();
-//   const [incidents, setIncidents] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchIncidents = async () => {
-//       try {
-//         const response = await fetch("http://localhost:1337/api/incidents", {
-//           headers: {
-//             Authorization: `Bearer ${token}`,
-//           },
-//         });
-//         if (!response.ok) {
-//           throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         const { data } = await response.json();
-//         setIncidents(data || []);
-//       } catch (err) {
-//         setError(err.message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//     fetchIncidents();
-//   }, [token]);
-
-//   const activeIncidents = incidents.filter(
-//     (incident) => incident.status_incident?.trim() === "в работе"
-//   );
-//   const completedIncidents = incidents.filter(
-//     (incident) => incident.status_incident === "выполнена"
-//   );
-
-//   const formatDate = (dateString, timeString) => {
-//     if (!dateString || !timeString) return "Нет данных";
-//     const date = new Date(dateString);
-//     if (isNaN(date.getTime())) return "Некорректная дата";
-
-//     const [hours, minutes, seconds] = timeString.split(":").map(Number);
-//     if (isNaN(hours) || isNaN(minutes) || isNaN(seconds))
-//       return "Некорректное время";
-
-//     date.setHours(hours, minutes, seconds);
-
-//     const day = String(date.getDate()).padStart(2, "0");
-//     const month = String(date.getMonth() + 1).padStart(2, "0");
-//     const year = date.getFullYear();
-//     const formattedHours = String(date.getHours()).padStart(2, "0");
-//     const formattedMinutes = String(date.getMinutes()).padStart(2, "0");
-
-//     return `${day}.${month}.${year} ${formattedHours}:${formattedMinutes}`;
-//   };
-
-//   const extractText = (field) => {
-//     if (!field || !Array.isArray(field)) return "Нет данных";
-//     return (
-//       field[0]?.children?.map((child) => child.text).join(" ") || "Нет данных"
-//     );
-//   };
-
-//   const renderActiveIncidents = () => {
-//     return (
-//       <Collapse className={styles.tabContent}>
-//         {activeIncidents.map((incident) => (
-//           <Panel
-//             key={incident.id}
-//             header={`ТН №${incident.id}`}
-//             className={styles.panelHeader}
-//           >
-//             <div className={styles.panelBody}>
-//               <Row gutter={[0, 10]}>
-//                 <Col span={24}>
-//                   <strong>Дата начала:</strong>{" "}
-//                   {formatDate(incident.start_date, incident.start_time)}
-//                 </Col>
-//                 <Col span={24}>
-//                   <strong>Описание:</strong> {extractText(incident.description)}
-//                 </Col>
-//                 <Col span={24}>
-//                   <strong>Прогноз восстановления:</strong>{" "}
-//                   {formatDate(
-//                     incident.estimated_restoration_time?.split("T")[0],
-//                     incident.estimated_restoration_time?.split("T")[1]
-//                   )}
-//                 </Col>
-//               </Row>
-//             </div>
-//           </Panel>
-//         ))}
-//       </Collapse>
-//     );
-//   };
-
-//   const renderCompletedIncidents = () => {
-//     return (
-//       <Collapse className={styles.tabContent}>
-//         {completedIncidents.map((incident) => {
-//           const start = new Date(
-//             `${incident.start_date}T${incident.start_time}`
-//           );
-//           const end = new Date(`${incident.end_date}T${incident.end_time}`);
-//           const durationHours = Math.round((end - start) / (1000 * 60 * 60));
-
-//           return (
-//             <Panel
-//               key={incident.id}
-//               header={`ТН №${incident.id}`}
-//               className={styles.panelHeader}
-//             >
-//               <div className={styles.panelBody}>
-//                 <Row gutter={[0, 10]}>
-//                   <Col span={24}>
-//                     <strong>Дата начала:</strong>{" "}
-//                     {formatDate(incident.start_date, incident.start_time)}
-//                   </Col>
-//                   <Col span={24}>
-//                     <strong>Описание:</strong>{" "}
-//                     {extractText(incident.description)}
-//                   </Col>
-//                   <Col span={24}>
-//                     <strong>Дата окончания:</strong>{" "}
-//                     {formatDate(incident.end_date, incident.end_time)}
-//                   </Col>
-//                   <Col span={24}>
-//                     <strong>Продолжительность:</strong> {durationHours} часов
-//                   </Col>
-//                   <Col span={24}>
-//                     <strong>Описание закрытия:</strong>{" "}
-//                     {extractText(incident.closure_description)}
-//                   </Col>
-//                 </Row>
-//               </div>
-//             </Panel>
-//           );
-//         })}
-//       </Collapse>
-//     );
-//   };
-
-//   if (loading) {
-//     return (
-//       <div style={{ textAlign: "center", marginTop: 50 }}>
-//         <Spin size="large" />
-//       </div>
-//     );
-//   }
-
-//   if (error) {
-//     return (
-//       <div style={{ marginTop: 50 }}>
-//         <Alert
-//           message="Ошибка загрузки данных"
-//           description={error}
-//           type="error"
-//         />
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <Container>
-//       <div style={{ textAlign: "right", marginTop: 20, marginBottom: 20 }}>
-//         <Button type="primary" danger onClick={logout}>
-//           Выйти
-//         </Button>
-//       </div>
-//       <Title level={1} className={styles.title}>
-//         Технологические нарушения
-//       </Title>
-//       <Tabs defaultActiveKey="1" className={styles.tabContent}>
-//         <Tabs.TabPane tab={`Начало ТН (${activeIncidents.length})`} key="1">
-//           {renderActiveIncidents()}
-//         </Tabs.TabPane>
-//         <Tabs.TabPane
-//           tab={`Окончание ТН (${completedIncidents.length})`}
-//           key="2"
-//         >
-//           {renderCompletedIncidents()}
-//         </Tabs.TabPane>
-//       </Tabs>
-//     </Container>
-//   );
-// };
-
-// export default MainContent;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,229 +1,37 @@
 "use client";
-import { useEffect } from "react";
+import Image from "next/image";
 import useAuthStore from "../stores/authStore";
 import AuthForm from "../components/AuthForm";
-import MainContent from "./MainContent";
+import MainContent from "../components/MainContent/MainContent";
 
 const Main = () => {
   const { token } = useAuthStore();
 
-  if (!token) return <AuthForm />;
-  return <MainContent />;
+  return (
+    <>
+      <div
+        style={{ textAlign: "center", marginTop: "20px", marginBottom: "20px" }}
+      >
+        <Image src="/logoBlue.svg" alt="Логотип" width={300} height={150} />
+      </div>
+      {!token ? <AuthForm /> : <MainContent />}
+    </>
+  );
 };
 
 export default Main;
 
 // "use client";
-// import React, { useEffect, useState } from "react";
-// import { Spin, Alert, Card, List, Tabs } from "antd";
-// import IdTN from "../components/CardComponent/Id";
-// import StartDate from "../components/CardComponent/StartDate";
-// import Description from "../components/CardComponent/Description";
-// import EstimatedRestorationTime from "../components/CardComponent/EstimatedRestorationTime";
+// import { useEffect } from "react";
+// import useAuthStore from "../stores/authStore";
+// import AuthForm from "../components/AuthForm";
+// import MainContent from "../components/MainContent/MainContent";
 
 // const Main = () => {
-//   const [incidents, setIncidents] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
+//   const { token } = useAuthStore();
 
-//   const API_TOKEN =
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzQwMzg5NjI1LCJleHAiOjE3NDI5ODE2MjV9.HAJOf5jjSMJT_srgpIy05hEiBCcnRNMnhhjQnMNd5Zc";
-
-//   useEffect(() => {
-//     const fetchIncidents = async () => {
-//       try {
-//         const response = await fetch("http://localhost:1337/api/incidents", {
-//           headers: {
-//             Authorization: `Bearer ${API_TOKEN}`,
-//           },
-//         });
-//         if (!response.ok)
-//           throw new Error(`HTTP error! status: ${response.status}`);
-//         const data = await response.json();
-//         setIncidents(data.data || []);
-//       } catch (err) {
-//         setError(err.message);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-//     fetchIncidents();
-//   }, []);
-
-//   const activeIncidents = incidents.filter(
-//     (incident) => incident.status_incident === "в работе"
-//   );
-//   const completedIncidents = incidents.filter(
-//     (incident) => incident.status_incident === "выполнена"
-//   );
-
-//   if (loading)
-//     return (
-//       <Spin size="large" style={{ display: "block", margin: "40px auto" }} />
-//     );
-//   if (error)
-//     return (
-//       <Alert
-//         message="Ошибка загрузки данных"
-//         description={error}
-//         type="error"
-//         style={{ margin: 20 }}
-//       />
-//     );
-
-//   const formatDate = (dateString, timeString) => {
-//     if (!dateString || !timeString) return "Нет данных";
-
-//     const date = new Date(dateString);
-//     if (isNaN(date.getTime())) return "Некорректная дата";
-
-//     const [hours, minutes, seconds] = timeString.split(":").map(Number);
-//     if (isNaN(hours) || isNaN(minutes) || isNaN(seconds))
-//       return "Некорректное время";
-
-//     date.setHours(hours, minutes, seconds);
-
-//     const day = String(date.getDate()).padStart(2, "0");
-//     const month = String(date.getMonth() + 1).padStart(2, "0");
-//     const year = date.getFullYear();
-//     const formattedHours = String(date.getHours()).padStart(2, "0");
-//     const formattedMinutes = String(date.getMinutes()).padStart(2, "0");
-
-//     return `${day}.${month}.${year} ${formattedHours}:${formattedMinutes}`;
-//   };
-
-//   const extractText = (field) => {
-//     if (!field || !Array.isArray(field)) return "Нет данных";
-//     return (
-//       field[0]?.children?.map((child) => child.text).join(" ") || "Нет данных"
-//     );
-//   };
-
-//   return (
-//     <div style={{ padding: "10px", maxWidth: "100vw", overflowX: "auto" }}>
-//       <Tabs
-//         defaultActiveKey="1"
-//         style={{ marginBottom: 0 }}
-//         items={[
-//           {
-//             key: "1",
-//             label: (
-//               <span style={{ fontSize: "1.5em", fontWeight: 500 }}>
-//                 Начало ТН ({activeIncidents.length})
-//               </span>
-//             ),
-//             children: (
-//               <section style={{ marginTop: 10 }}>
-//                 <h2 style={{ fontSize: "1.8em", marginBottom: 10 }}>
-//                   Активные технологические нарушения
-//                 </h2>
-//                 <List
-//                   dataSource={activeIncidents}
-//                   renderItem={(incident) => (
-//                     <List.Item style={{ padding: "5px 0" }}>
-//                       <Card
-//                         style={{
-//                           width: "100%",
-//                           display: "flex",
-//                           flexDirection: "row",
-//                           alignItems: "center",
-//                           backgroundColor: "#fffbe6",
-//                           padding: "10px",
-//                           margin: 0,
-//                         }}
-//                       >
-//                         <IdTN id={incident.id} />
-//                         <StartDate
-//                           date={formatDate(
-//                             incident.start_date,
-//                             incident.start_time
-//                           )}
-//                         />
-//                         <Description text={extractText(incident.description)} />
-//                         <EstimatedRestorationTime
-//                           date={
-//                             incident.estimated_restoration_time
-//                               ? formatDate(
-//                                   incident.estimated_restoration_time.split(
-//                                     "T"
-//                                   )[0],
-//                                   incident.estimated_restoration_time.split(
-//                                     "T"
-//                                   )[1]
-//                                 )
-//                               : "Нет данных"
-//                           }
-//                         />
-//                       </Card>
-//                     </List.Item>
-//                   )}
-//                 />
-//               </section>
-//             ),
-//           },
-//           {
-//             key: "2",
-//             label: (
-//               <span style={{ fontSize: "1.5em", fontWeight: 500 }}>
-//                 Окончание ТН ({completedIncidents.length})
-//               </span>
-//             ),
-//             children: (
-//               <section style={{ marginTop: 10 }}>
-//                 <h2 style={{ fontSize: "1.8em", marginBottom: 10 }}>
-//                   Завершенные технологические нарушения
-//                 </h2>
-//                 <List
-//                   dataSource={completedIncidents}
-//                   renderItem={(incident) => (
-//                     <List.Item style={{ padding: "5px 0" }}>
-//                       <Card
-//                         style={{
-//                           width: "100%",
-//                           display: "flex",
-//                           flexDirection: "row",
-//                           alignItems: "center",
-//                           backgroundColor: "#f6ffed",
-//                           padding: "10px",
-//                           margin: 0,
-//                         }}
-//                       >
-//                         <IdTN id={incident.id} />
-//                         <StartDate
-//                           date={formatDate(
-//                             incident.end_date,
-//                             incident.end_time
-//                           )}
-//                         />
-//                         <Description
-//                           text={extractText(incident.closure_description)}
-//                         />
-//                         <div style={{ flex: 2 }}>
-//                           <p>
-//                             <strong>Продолжительность:</strong>{" "}
-//                             {Math.round(
-//                               (new Date(
-//                                 `${incident.end_date}T${incident.end_time}`
-//                               ) -
-//                                 new Date(
-//                                   `${incident.start_date}T${incident.start_time}`
-//                                 )) /
-//                                 (1000 * 60 * 60)
-//                             )}{" "}
-//                             часов
-//                           </p>
-//                         </div>
-//                       </Card>
-//                     </List.Item>
-//                   )}
-//                 />
-//               </section>
-//             ),
-//           },
-//         ]}
-//       />
-//     </div>
-//   );
+//   if (!token) return <AuthForm />;
+//   return <MainContent />;
 // };
 
 // export default Main;
